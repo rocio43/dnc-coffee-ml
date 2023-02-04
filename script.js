051035ddb051f3e874bc1fe22e47ed211be228f3
rocio43 <98287489+rocio43@users.noreply.github.com>
2023-02-03 21:09:43 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// CARD 1
+document.getElementById("card01").onmouseover = function () { mouseOver1() };
+document.getElementById("card01").onmouseout = function () { mouseOut1() };
+
+function mouseOver1() { document.getElementById("btn01").style.visibility = 'visible' };
+function mouseOut1() { document.getElementById("btn01").style.visibility = 'hidden' };
+
+// CARD 2
+document.getElementById("card02").onmouseover = function () { mouseOver2() };
+document.getElementById("card02").onmouseout = function () { mouseOut2() };
+
+function mouseOver2() { document.getElementById("btn02").style.visibility = 'visible' };
+function mouseOut2() { document.getElementById("btn02").style.visibility = 'hidden' };
+
+// CARD 3
+document.getElementById("card03").onmouseover = function () { mouseOver3() };
+document.getElementById("card03").onmouseout = function () { mouseOut3() };
+
+function mouseOver3() { document.getElementById("btn03").style.visibility = 'visible' };
+function mouseOut3() { document.getElementById("btn03").style.visibility = 'hidden' };
+
+
+// CARD 4
+document.getElementById("card04").onmouseover = function () { mouseOver4() };
+document.getElementById("card04").onmouseout = function () { mouseOut4() };
+
+function mouseOver4() { document.getElementById("btn04").style.visibility = 'visible' };
+function mouseOut4() { document.getElementById("btn04").style.visibility = 'hidden' };
+
+
